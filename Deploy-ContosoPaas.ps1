f6dbb6ec4d750b7e092e1f485fb7abf301234dbc
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-05-25 18:09:37 +0400

    initial commit

---
+++ Deploy-ContosoPaas.ps1
@@ -0,0 +1,25 @@
+﻿### Define Deployment Variables
+
+$resourceGrouplocation = 'Australia Southeast'
+$resourceGroupName = 'contoso-paas'
+$resourceDeploymentName = 'contoso-paas-deployment'
+$templatePath = 'D:\Desktop_Files\Powershell\webapp'
+$templateFile = 'contosoPaas.json'
+$template = $templatePath + '\' + $templateFile
+
+
+### Create Resource Group
+
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $resourcegrouplocation `
+    -Verbose -Force
+
+
+### Deploy Resources
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
